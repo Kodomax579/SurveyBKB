b1682e074505ddb05fbe02590f68bbe721a8aff8
John <braun@swwaf.de>
2026-04-30 11:02:03 +0200

    NewsCard, NewsItem und NewsPage erstellt

---
+++ SchulFunk_Webprojekt/Model/NewsItem.cs
@@ -0,0 +1,13 @@
+﻿namespace SchulFunk_Webprojekt.Model
+{
+    public class NewsItem
+    {
+        public string Tag { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Date { get; set; } = "";
+        public string Artikel { get; set; } = "";
+        public string IconPath { get; set; } = "lib/images/Icon-News.png";
+        public byte[] Image { get; set; } = Array.Empty<byte>();
+    }
+}
