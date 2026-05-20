fc4846d2eeec2fa67ec59d9da69184dce04c3037
Carsten <engelhardtcarsten00@gmail.com>
2026-05-20 23:51:08 +0200

    remove unused mikroservices

--- SurveyService/Data/AnswerModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SurveyService.Data
-{
-    public class AnswerModel
-    {
-        public int Id { get; set; }
-        public string Answer { get; set; }
-        public int NumberOfSelectedAnswer { get; set; }
-    }
-}
