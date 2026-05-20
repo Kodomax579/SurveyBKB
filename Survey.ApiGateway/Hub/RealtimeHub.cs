7956268be4e0e0b3f9d2f99747cca8535c296c93
carsten <engelhardtcarsten00@gmail.com>
2026-05-20 09:20:30 +0200

    signalR hinzufügen

---
+++ Survey.ApiGateway/Hub/RealtimeHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Survey.ApiGateway.Models;
+
+namespace Survey.ApiGateway.RealtimeHub
+{
+    public class RealtimeHub : Hub
+    {
+        public async Task SendNewNews(List<NewsModel> news)
+        {
+            await Clients.All.SendAsync("NewNews", news);
+        }
+
+        public async Task SendNewSurvey(List<SurveyModel> survey)
+        {
+            await Clients.All.SendAsync("NewSurvey", survey);
+        }
+    }
+}
