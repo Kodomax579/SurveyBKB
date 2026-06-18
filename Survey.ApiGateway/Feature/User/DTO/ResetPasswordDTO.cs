dde3dec8e78e04899c20f3f39fe637418ad7f0c6
Carsten <engelhardtcarsten00@gmail.com>
2026-06-18 17:27:34 +0200

    n

--- Survey.ApiGateway/Feature/User/DTO/ResetPasswordDTO.cs
+++ Survey.ApiGateway/Feature/User/DTO/ResetPasswordDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace Survey.ApiGateway.Feature.User.DTO
+﻿using System.Text.Json.Serialization;
+
+namespace Survey.ApiGateway.Feature.User.DTO
 {
     public class ResetPasswordDTO
     {
+        [JsonPropertyName("email")]
         public string Email { get; set; } = string.Empty;
+
+        [JsonPropertyName("password")]
         public string NewPassword { get; set; } = string.Empty;
     }
 }
